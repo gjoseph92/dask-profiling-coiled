95d78d80e247b5312d8e9070617c89574be8477d
Gabe Joseph <gjoseph92@gmail.com>
2021-05-19 18:35:25 -0600

    coiled notebook

--- make-coiled-env.sh
+++ make-coiled-env.sh
@@ -4,7 +4,9 @@
 cat > postbuild.sh <<EOF
 #!/bin/bash
 
-python3 -m pip install git+https://github.com/gjoseph92/scheduler-profilers.git@c3e02d33ab2bf7e99ff920563b38dfd92b2ce65b
+python3 -m pip install git+https://github.com/gjoseph92/scheduler-profilers.git@2691c0fc79e4f4fc9e90c7cfcbdf153f45107d36
 EOF
 coiled env create -n profiling --conda environment.yml --post-build postbuild.sh
 rm postbuild.sh
+
+python make-coiled-notebook.py
---
+++ make-coiled-notebook.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import coiled
+
+if __name__ == "__main__":
+    coiled.create_job_configuration(
+        name="profiling",
+        software="gjoseph92/profiling",
+        cpu=1,
+        memory="4 GiB",
+        command=[
+            "/bin/bash",
+            "-c",
+            "SHELL=/bin/bash jupyter lab --allow-root --ip=0.0.0.0 --no-browser",
+        ],
+        ports=[8888],
+        files=["dask_profiling_coiled/run_profile.py"],
+    )
