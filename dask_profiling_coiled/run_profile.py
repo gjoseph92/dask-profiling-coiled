48d3b6aa8068e59fd63b8a1adcfbb21795907811
Gabe Joseph <gjoseph92@gmail.com>
2021-05-20 15:13:59 -0600

    cython-shuffle-nogc rerun from coiled job

--- dask_profiling_coiled/run_profile.py
+++ dask_profiling_coiled/run_profile.py
@@ -1,3 +1,4 @@
+import sys
 import time
 import pickle
 
@@ -8,7 +9,7 @@ import dask.dataframe
 import distributed
 import distributed.protocol
 
-# from scheduler_profilers import pyspy_on_scheduler
+from scheduler_profilers import pyspy_on_scheduler
 
 
 def print_sizeof_serialized_graph(x) -> None:
@@ -17,20 +18,30 @@ def print_sizeof_serialized_graph(x) -> None:
     optimize_time = time.perf_counter() - start
 
     start = time.perf_counter()
-    pickled = len(pickle.dumps(dsk))
+    packed = dsk.__dask_distributed_pack__(distributed.get_client(), x.__dask_keys__())
+    pack_time = time.perf_counter() - start
+
+    start = time.perf_counter()
+    frames = distributed.protocol.dumps(packed)
+    dumps_time = time.perf_counter() - start
+    dumps = sum(len(f) for f in frames)
+
+    start = time.perf_counter()
+    pickled = len(pickle.dumps(packed))
     pickle_time = time.perf_counter() - start
 
     print(
         f"Graph ({len(dsk)} optimized tasks) is:\n"
+        f"* {dask.utils.format_bytes(dumps)} with distributed-dumps ({len(frames)} frames) - {dumps_time:.1}s\n"
         f"* {dask.utils.format_bytes(pickled)} pickled  - {pickle_time:.1}s\n"
-        f"Optimize: {optimize_time:.1}s"
+        f"Optimize: {optimize_time:.1}s, pack: {pack_time:.1}s"
     )
 
 
 def main():
     df = dask.datasets.timeseries(
         start="2000-01-01",
-        end="2000-06-30",  # 4,344 partitions
+        end="2000-06-30",  # 720 ~partitions
         partition_freq="1h",
         freq="60s",
     )
@@ -61,19 +72,24 @@ if __name__ == "__main__":
         shutdown_on_close=True,
     )
     client = distributed.Client(cluster)
+    if not client.run_on_scheduler(lambda: distributed.scheduler.COMPILED):
+        print("Scheduler is not compiled!")
+        client.shutdown()
+        client.close()
+        sys.exit(1)
 
     print(f"Waiting for {n_workers} workers...")
     client.wait_for_workers(n_workers)
 
-    # def disable_gc():
-    #     # https://github.com/benfred/py-spy/issues/389#issuecomment-833903190
-    #     import gc
+    def disable_gc():
+        # https://github.com/benfred/py-spy/issues/389#issuecomment-833903190
+        import gc
 
-    #     gc.disable()
-    #     gc.set_threshold(0)
+        gc.disable()
+        gc.set_threshold(0)
 
-    # print("Disabling GC on scheduler")
-    # client.run_on_scheduler(disable_gc)
+    print("Disabling GC on scheduler")
+    client.run_on_scheduler(disable_gc)
 
     # def enable_gc_debug():
     #     import gc
@@ -85,15 +101,18 @@ if __name__ == "__main__":
 
     print("Here we go!")
 
-    test_name = "2.30-shuffle-gc-nopyspy"
+    # This is key---otherwise we're uploading ~300MiB of graph to the scheduler
+    dask.config.set({"optimization.fuse.active": False})
+
+    test_name = "cython-shuffle-nogc"
     with (
         distributed.performance_report(f"results/{test_name}.html"),
-        # pyspy_on_scheduler(
-        #     f"results/{test_name}.json",
-        #     subprocesses=True,
-        #     idle=True,
-        #     native=True,
-        # ),
+        pyspy_on_scheduler(
+            f"results/{test_name}.json",
+            subprocesses=True,
+            idle=True,
+            native=True,
+        ),
     ):
         main()
 
