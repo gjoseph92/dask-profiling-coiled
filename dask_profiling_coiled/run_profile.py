6de1bef4624859f13f0dcb830054b81c95ed89b2
Gabe Joseph <gjoseph92@gmail.com>
2021-06-11 11:20:55 -0600

    cython-shuffle-gc-500ms-batched-send

--- dask_profiling_coiled/run_profile.py
+++ dask_profiling_coiled/run_profile.py
@@ -95,15 +95,15 @@ if __name__ == "__main__":
         pass
     client.wait_for_workers(n_workers)
 
-    def disable_gc():
-        # https://github.com/benfred/py-spy/issues/389#issuecomment-833903190
-        import gc
+    # def disable_gc():
+    #     # https://github.com/benfred/py-spy/issues/389#issuecomment-833903190
+    #     import gc
 
-        gc.disable()
-        gc.set_threshold(0)
+    #     gc.disable()
+    #     gc.set_threshold(0)
 
-    print("Disabling GC on scheduler")
-    client.run_on_scheduler(disable_gc)
+    # print("Disabling GC on scheduler")
+    # client.run_on_scheduler(disable_gc)
 
     # def enable_gc_debug():
     #     import gc
@@ -123,7 +123,7 @@ if __name__ == "__main__":
         }
     )
 
-    test_name = "cython-shuffle-nogc-500ms-batched-send"
+    test_name = "cython-shuffle-gc-500ms-batched-send"
     with (
         distributed.performance_report(f"results/{test_name}.html"),
         pyspy_on_scheduler(
