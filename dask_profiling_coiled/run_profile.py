fc21c153ddaeab3efa4ba80a22a83f72af7d8325
Gabe Joseph <gjoseph92@gmail.com>
2021-07-02 13:14:28 -0600

    purepy-shuffle-nogc

--- dask_profiling_coiled/run_profile.py
+++ dask_profiling_coiled/run_profile.py
@@ -81,15 +81,15 @@ if __name__ == "__main__":
         pass
     client.wait_for_workers(n_workers)
 
-    # def disable_gc():
-    #     # https://github.com/benfred/py-spy/issues/389#issuecomment-833903190
-    #     import gc
+    def disable_gc():
+        # https://github.com/benfred/py-spy/issues/389#issuecomment-833903190
+        import gc
 
-    #     gc.disable()
-    #     gc.set_threshold(0)
+        gc.disable()
+        gc.set_threshold(0)
 
-    # print("Disabling GC on scheduler")
-    # client.run_on_scheduler(disable_gc)
+    print("Disabling GC on scheduler")
+    client.run_on_scheduler(disable_gc)
 
     print("[bold green]Here we go!")
 
@@ -103,7 +103,7 @@ if __name__ == "__main__":
     )
 
     n_trials = 10
-    test_name = "purepy-shuffle-gc"
+    test_name = "purepy-shuffle-nogc"
     trials = [trial(client, i) for i in range(n_trials)]
 
     print("[bold green]Trials complete!")
